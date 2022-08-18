039e78e47ed25aa7908363cd098a961df32bbe82
prime97 <primesimon440@gmail.com>
2022-08-18 17:40:04 +0100

    Create Skillsmod.js

---
+++ src/components/Skillsmod/Skillsmod.js
@@ -0,0 +1,55 @@
+
+import tw, {styled} from "twin.macro";
+
+
+export const SkillSection = styled.div(() =>[
+    tw`max-w-4xl mx-auto justify-center py-12`,
+]);
+
+export const SkillFrame = styled.div(() =>[
+    tw`flex flex-wrap justify-center pt-2`,
+]);
+
+export const Skillborders = styled.div(() =>[
+    tw`flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52`,
+]);
+
+export const P1 = styled.div(() =>[
+    tw`mt-6 text-xl sm:text-2xl font-semibold text-center`
+]);
+
+export const P2 = styled.div(() =>[
+    tw`text-2xl text-black text-center sm:text-4xl pt-4 font-bold`
+]);
+
+export const CPlusPlusLogo = styled.div(() =>[
+    tw`mx-auto text-4xl text-blue-500`,
+]);
+
+export const JavaLogo = styled.div(() =>[
+    tw`mx-auto text-4xl text-red-600`,
+]);
+
+export const PythonLogo = styled.div(() =>[
+    tw`mx-auto text-4xl text-yellow-500`,
+]);
+
+export const HTMLLogo = styled.div(() =>[
+    tw`mx-auto text-4xl text-yellow-500`,
+]);
+
+export const MySQLLogo = styled.div(() =>[
+    tw`mx-auto text-4xl text-yellow-400`,
+]);
+
+export const JavaScriptLogo = styled.div(() =>[
+    tw`mx-auto text-4xl text-yellow-500`,
+]);
+
+export const TailwindCSSLogo = styled.div(() =>[
+    tw`mx-auto text-4xl text-blue-400`,
+]);
+
+export const ReactLogo = styled.div(() =>[
+    tw`mx-auto text-4xl text-purple-700`,
+]);
