81b05342edbb9409e09ea716875be7ecc1981ab3
prime97 <primesimon440@gmail.com>
2022-08-18 17:40:06 +0100

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const info = {
+    about:"I am Siddique, student of 3rd year BSc Computer Science, in London Metropolitan Uniersity. I am a passionate programmer who loves to learn new programming languages and currently learning how to develop web app using react. Since I started my journey at my Uni, I participated in a java programming competition and won the 1st prize. Apart from it, I made some basic Java Applications and a website using JavaBeans, SQL and glassfish server for my Uni assignments. I also was a success coach in Uni for the foundation and 1st year students. I helped them with their programming modules and tasks.",
+    short_line: "Coding languages I know"
+};
+
+export { info };
