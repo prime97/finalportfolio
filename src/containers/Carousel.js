2cff0007ac0b0a1423ce781df0ddb55a6c5c9d4e
prime97 <primesimon440@gmail.com>
2022-08-18 23:58:53 +0100

    Create Carousel.js

---
+++ src/containers/Carousel.js
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/pagination";
+import {Pagination} from "swiper";
+import {Frame} from "../components/Carouselmods"
+function Carousel () {
+  return (
+    
+    
+    <Frame>
+        <>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+    </>
+    </Frame>
+  )
+}
+
+export default Carousel;
