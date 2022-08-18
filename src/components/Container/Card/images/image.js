9d54bb5397c21dec4984818f320f35532312b454
prime97 <primesimon440@gmail.com>
2022-08-18 17:39:41 +0100

    Create image.js

---
+++ src/components/Container/Card/images/image.js
@@ -0,0 +1,9 @@
+import tw, {styled} from "twin.macro";
+
+export const ImageDiv = styled.div(() =>[
+    tw``,
+]);
+
+export const ProfileImage = styled.img(() =>[
+    tw`w-32 mx-auto shadow-xl rounded-full drop-shadow-sm`,
+]);
